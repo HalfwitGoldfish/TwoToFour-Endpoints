4d9bc76e7dc54209c7fcb38dcdd92ec2225d0d9f
HalfwitGoldfish <Chas2687@gmail.com>
2025-01-07 15:05:51 -0800

    removed uneeded code

--- Services/GreaterOrLessServices.cs
+++ Services/GreaterOrLessServices.cs
@@ -5,13 +5,9 @@ namespace TwoToFour_Endpoints.Services
         public string GreaterOrLess(int num1, int num2)
         {
             if(num1 < num2){
-                string firstNum = $"{num1} is less than {num2}";
-                string secondNum = $"{num2} is greater than {num1}";
-                return $"{firstNum}\n{secondNum}";
+                return $"{num1} is less than {num2}\n{num2} is greater than {num1}";
             }else if(num1 > num2){
-                string firstNum = $"{num1} is greater than {num2}";
-                string secondNum = $"{num2} is less than {num1}";
-                return $"{firstNum}\n{secondNum}";
+                return $"{num1} is greater than {num2}\n{num2} is less than {num1}";
             }else{
                 return $"{num1} is equal to {num2}";
             }
