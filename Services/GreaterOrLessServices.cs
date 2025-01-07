1c7b8c8947a9d5b1ffa075144c9c8fc830941aea
HalfwitGoldfish <Chas2687@gmail.com>
2025-01-07 14:29:02 -0800

    Removed unneded code and created read me

--- Services/GreaterOrLessServices.cs
+++ Services/GreaterOrLessServices.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.ObjectPool;
-
 namespace TwoToFour_Endpoints.Services
 {
     public class GreaterOrLessServices
--- Services/NameAndTimeServices.cs
+++ Services/NameAndTimeServices.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace TwoToFour_Endpoints.Services
 {
     public class NameAndTimeServices
